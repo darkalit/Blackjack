3f858402045404112ad935c3a8115afc0daa6933
darkalit <tkachenkozen@gmail.com>
2024-11-26 19:43:23 +0200

    Add sounds and music to resource manager

--- Source/ResourceManager.cpp
+++ Source/ResourceManager.cpp
@@ -1,8 +1,12 @@
 #include "ResourceManager.h"
 
+#include "Music.h"
+#include "Sound.h"
 #include "Texture.h"
 
 std::unordered_map<std::string, Texture> ResourceManager::m_Textures = {};
+std::unordered_map<std::string, Music> ResourceManager::m_Musics = {};
+std::unordered_map<std::string, Sound> ResourceManager::m_Sounds = {};
 
 Texture& ResourceManager::AddTexture(const std::string& filepath)
 {
@@ -18,7 +22,54 @@ Texture& ResourceManager::AddTexture(const std::string& filepath)
     return m_Textures.at(filepath);
 }
 
+void ResourceManager::RemoveTexture(const std::string& filepath)
+{
+    auto pair = m_Textures.find(filepath);
+    if (pair != m_Textures.end())
+    {
+        m_Textures.erase(pair);
+    }
+}
+
 Texture& ResourceManager::GetTexture(const std::string& filepath)
 {
     return m_Textures.at(filepath);
+}
+
+Music& ResourceManager::AddMusic(const std::string& filepath)
+{
+    auto pair = m_Musics.find(filepath);
+    if (pair != m_Musics.end())
+    {
+        return pair->second;
+    }
+
+    auto music = Music(filepath);
+    m_Musics.try_emplace(filepath, std::move(music));
+
+    return m_Musics.at(filepath);
+}
+
+Music& ResourceManager::GetMusic(const std::string& filepath)
+{
+    return m_Musics.at(filepath);
+}
+
+Sound& ResourceManager::AddSound(const std::string& filepath)
+{
+    auto pair = m_Sounds.find(filepath);
+    if (pair != m_Sounds.end())
+    {
+        return pair->second;
+    }
+
+    auto sound = Sound(filepath);
+    m_Sounds.try_emplace(filepath, std::move(sound));
+
+    return m_Sounds.at(filepath);
+}
+
+Sound& ResourceManager::GetSound(const std::string& filepath)
+{
+    return m_Sounds.at(filepath);
 }
--- Source/ResourceManager.h
+++ Source/ResourceManager.h
@@ -4,6 +4,8 @@
 #include <unordered_map>
 
 class Texture;
+class Music;
+class Sound;
 
 class ResourceManager
 {
@@ -12,9 +14,18 @@ public:
     ~ResourceManager() = delete;
 
     static Texture& AddTexture(const std::string& filepath);
+    static void RemoveTexture(const std::string& filepath);
     static Texture& GetTexture(const std::string& filepath);
+
+    static Music& AddMusic(const std::string& filepath);
+    static Music& GetMusic(const std::string& filepath);
+
+    static Sound& AddSound(const std::string& filepath);
+    static Sound& GetSound(const std::string& filepath);
 
 private:
     static std::unordered_map<std::string, Texture> m_Textures;
+    static std::unordered_map<std::string, Music> m_Musics;
+    static std::unordered_map<std::string, Sound> m_Sounds;
 };
 
